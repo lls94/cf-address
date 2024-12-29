6eac55d26ec43fa21d55fc73a4c215fd85e6ffdc
李力士 <lilishi@chinasofti.com>
2024-12-29 12:40:47 +0800

    自动提交生成的代码

--- main.bat
+++ main.bat
@@ -1,7 +1,7 @@
 chcp 65001
 @echo off
 @REM go run main.go -p 0 -tll 40 -tl 500 -httping -cfcolo LAX,HKG,NRT,ICN,DXB,SIN,NYC,SFO,KIX,BOM,DEL,JKT,KUL,TPE -dd
-go run main.go -p 0 -tll 40 -tl 300 -httping -cfcolo * -dd
+go run main.go -p 0 -tll 40 -tl 260 -httping -cfcolo * -dd
 git add .
 git commit -m "自动提交生成的代码"
 git push
